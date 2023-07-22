a6ede7c511de126056ac9d2a003d73244e1b517c
IweZix <IweZix@users.noreply.github.com>
2023-07-22 12:06:37 +0200

    v2.0.1

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "YOUR_TOKEN",
+    token: "MTA0OTM5NjY4NDA3NTA1MzA3Nw.Gd9WBH.uxNJGnDRnMbP9YDrTo9uLZSsgDTRuRx7Hb5Pys",
     ownerID: "YOUR_DISCORD_ID",
     version: "2.0.0",
 }
