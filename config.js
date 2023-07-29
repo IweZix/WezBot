f847e18eff251b4ab9c42d62512e989013e9d754
IweZix <lucanclv2@gmail.com>
2023-07-29 21:23:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -4,10 +4,3 @@
     version: "2.0.0",
     color: "#FF7300"
 }*/
-
-module.exports = {
-    token: "MTA0OTM5NjY4NDA3NTA1MzA3Nw.G3oAxw.5etvsW3WN0az43gwh5Jby2omgiPcG3uDGCWM-o",
-    ownerID: "914162418660302889",
-    version: "2.1.0",
-    color: "#FF7300"
-}
