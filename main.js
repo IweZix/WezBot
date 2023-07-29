cef242de9f9aebcec6064d2506f7816a14a3596e
IweZix <lucanclv2@gmail.com>
2023-07-29 21:13:27 +0200

    v2.1.1

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-const Discord = require("discord.js")
-const intents = new Discord.IntentsBitField(3276799)
-const bot = new Discord.Client({intents})
-const loadCommands = require("./Loaders/loadCommands")
-const loadEvents = require("./Loaders/loadEvents")
-const config = require("./config")
+const Discord = require("discord.js");
+const intents = new Discord.IntentsBitField(3276799);
+const bot = new Discord.Client({intents});
+const loadCommands = require("./Loaders/loadCommands");
+const loadEvents = require("./Loaders/loadEvents");
+const config = require("./config");
 
-bot.commands = new Discord.Collection()
-bot.fonction = {
+bot.commands = new Discord.Collection();
+/*bot.fonction = {
     createId: require("./Fonctions/createId"),
     calculXP: require("./Fonctions/calculXP")
-}
+}*/
 bot.color = "0xff7300"
 
 bot.login(config.token)
@@ -19,5 +19,7 @@ process.on('unhandledRejection', error => {
     else console.error('Unhandled promise rejection:', error);
 });
 
-loadCommands(bot)
-loadEvents(bot)
+// base
+// loadCommands(bot);
+loadCommands(bot, process.cwd() + '/Commandes');
+loadEvents(bot);
